fa79e4ea8795760b6a59e12ed2da759b6b917c9f
Alex Ormenisan <ormenisan.adrian@gmail.com>
2020-01-10 14:18:18 +0100

    cleanup refactor and rspec test

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/provenance/ProvenanceCleaner.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/provenance/ProvenanceCleaner.java
@@ -15,27 +15,18 @@
  */
 package io.hops.hopsworks.api.provenance;
 
-import io.hops.hopsworks.common.dao.hdfs.inode.Inode;
-import io.hops.hopsworks.common.dao.hdfs.inode.InodeFacade;
-import io.hops.hopsworks.common.dao.project.Project;
-import io.hops.hopsworks.common.dao.project.ProjectFacade;
-import io.hops.hopsworks.common.provenance.core.elastic.ProvElasticController;
+import io.hops.hopsworks.common.provenance.core.ProvenanceCleanerController;
 import io.hops.hopsworks.common.util.Settings;
-import io.hops.hopsworks.exceptions.ElasticException;
-import io.hops.hopsworks.exceptions.ProvenanceException;
-import io.hops.hopsworks.restutils.RESTCodes;
-import org.elasticsearch.ElasticsearchException;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.support.master.AcknowledgedResponse;
-import org.elasticsearch.client.indices.GetIndexRequest;
-import org.elasticsearch.client.indices.GetIndexResponse;
-import org.elasticsearch.rest.RestStatus;
 import org.javatuples.Pair;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 import javax.ejb.EJB;
-import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import javax.ejb.Timeout;
 import javax.ejb.Timer;
+import javax.ejb.TimerService;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import java.util.logging.Level;
@@ -46,99 +37,41 @@ import java.util.logging.Logger;
 public class ProvenanceCleaner {
   private final static Logger LOGGER = Logger.getLogger(ProvenanceCleaner.class.getName());
   
+  @Resource
+  TimerService timerService;
   @EJB
   private Settings settings;
   @EJB
-  private ProvElasticController client;
-  @EJB
-  private ProjectFacade projectFacade;
-  @EJB
-  private InodeFacade inodeFacade;
+  private ProvenanceCleanerController cleanerCtrl;
   
   private String lastIndexChecked = "";
   
-  // Run once every four hours
-  @Schedule(persistent = false, hour = "*/4")
-  public void execute(Timer timer) {
-    LOGGER.log(Level.INFO, "Running ProvenanceCleaner.");
+  @PostConstruct
+  private void init() {
+    timerService.createTimer(0, settings.getProvCleanerPeriod(), "Timer for provenance cleaner.");
+  }
+  
+  @PreDestroy
+  private void destroyTimer() {
+    for (Timer timer : timerService.getTimers()) {
+      timer.cancel();
+    }
+  }
+  
+  @Timeout
+  @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+  private void timeout(Timer timer) {
+    int cleanupSize = settings.getProvCleanupSize();
+    if(cleanupSize == 0) {
+      return;
+    }
     try {
-      int cleanupSize = settings.getProvCleanupSize();
-      int archiveSize = settings.getProvArchiveSize();
-      if(archiveSize == 0) {
-        return;
-      }
-      Pair<Integer, String> round = archiveRound(lastIndexChecked, cleanupSize);
-      LOGGER.log(Level.INFO, "cleanup round - idx cleaned:{0} from:{1} to:{2}",
+      Pair<Integer, String> round = cleanerCtrl.indexCleanupRound(lastIndexChecked, cleanupSize);
+      LOGGER.log(Level.FINE, "cleanup round - idx cleaned:{0} from:{1} to:{2}",
         new Object[]{round.getValue0(), lastIndexChecked, round.getValue1()});
       lastIndexChecked = round.getValue1();
     } catch (Exception e) {
       LOGGER.log(Level.INFO, "cleanup round was not successful - error", e);
-    }
-  }
-  
-  private Pair<Integer, String> archiveRound(String nextToCheck, Integer limitIdx)
-    throws ProvenanceException, ElasticException {
-    String[] indices = getAllIndices();
-    
-    int cleaned = 0;
-    String nextToCheckAux = "";
-    for(String indexName : indices) {
-      if(cleaned > limitIdx) {
-        nextToCheckAux = indexName;
-        break;
-      }
-      if(indexName.compareTo(nextToCheck) < 0) {
-        continue;
-      }
-      Project project = getProject(indexName);
-      if(project == null) {
-        LOGGER.log(Level.INFO, "deleting prov index:{0} with no corresponding project", indexName);
-        deleteProvIndex(indexName);
-        cleaned++;
-        continue;
-      }
-    }
-    return Pair.with(cleaned, nextToCheckAux);
-  }
-  
-  private Project getProject(String indexName) throws ProvenanceException {
-    int endIndex = indexName.indexOf(Settings.PROV_FILE_INDEX_SUFFIX);
-    String sInodeId = indexName.substring(0, endIndex);
-    Long inodeId;
-    try {
-      inodeId = Long.parseLong(sInodeId);
-    }catch(NumberFormatException e) {
-      throw new ProvenanceException(RESTCodes.ProvenanceErrorCode.INTERNAL_ERROR, Level.WARNING,
-        "error extracting project from prov index name - format error", e.getMessage(), e);
-    }
-    Inode inode = inodeFacade.findById(inodeId);
-    if(inode == null) {
-      return null;
-    }
-    Project project = projectFacade.findByInodeId(inode.getInodePK().getParentId(), inode.getInodePK().getName());
-    return project;
-  }
-
-  private String[] getAllIndices() throws ElasticException {
-    String indexRegex = "*" + Settings.PROV_FILE_INDEX_SUFFIX;
-    GetIndexRequest request = new GetIndexRequest(indexRegex);
-    GetIndexResponse response = client.mngIndexGet(request);
-    return response.getIndices();
-  }
-  
-  private void deleteProvIndex(String indexName) throws ElasticException {
-    DeleteIndexRequest request = new DeleteIndexRequest(indexName);
-    try {
-      AcknowledgedResponse response = client.mngIndexDelete(request);
-    } catch (ElasticException e) {
-      if(e.getCause() instanceof ElasticsearchException) {
-        ElasticsearchException ex = (ElasticsearchException)e.getCause();
-        if(ex.status() == RestStatus.NOT_FOUND) {
-          LOGGER.log(Level.INFO, "trying to delete index:{0} - does not exist", indexName);
-          return;
-        }
-      }
-      LOGGER.log(Level.WARNING, "trying to delete index:{0}", e.getStackTrace());
     }
   }
 }
---
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/provenance/core/ProvenanceCleanerController.java
@@ -0,0 +1,109 @@
+package io.hops.hopsworks.common.provenance.core;
+
+import io.hops.hopsworks.common.dao.hdfs.inode.Inode;
+import io.hops.hopsworks.common.dao.hdfs.inode.InodeFacade;
+import io.hops.hopsworks.common.dao.project.Project;
+import io.hops.hopsworks.common.dao.project.ProjectFacade;
+import io.hops.hopsworks.common.provenance.core.elastic.ProvElasticController;
+import io.hops.hopsworks.common.util.Settings;
+import io.hops.hopsworks.exceptions.ElasticException;
+import io.hops.hopsworks.exceptions.ProvenanceException;
+import io.hops.hopsworks.restutils.RESTCodes;
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.rest.RestStatus;
+import org.javatuples.Pair;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.NEVER)
+public class ProvenanceCleanerController {
+  private final static Logger LOGGER = Logger.getLogger(ProvenanceCleanerController.class.getName());
+  
+  @EJB
+  private ProvElasticController client;
+  @EJB
+  private ProjectFacade projectFacade;
+  @EJB
+  private InodeFacade inodeFacade;
+  
+  public Pair<Integer, String> indexCleanupRound(String nextToCheck, Integer limit)
+    throws ProvenanceException {
+    String[] indices = getAllIndices();
+    
+    int cleaned = 0;
+    String nextToCheckAux = "";
+    for(String indexName : indices) {
+      if(cleaned > limit) {
+        nextToCheckAux = indexName;
+        break;
+      }
+      if(indexName.compareTo(nextToCheck) < 0) {
+        continue;
+      }
+      Project project = getProject(indexName);
+      if(project == null) {
+        LOGGER.log(Level.FINE, "deleting prov index:{0} with no corresponding project", indexName);
+        deleteProvIndex(indexName);
+        cleaned++;
+        continue;
+      }
+    }
+    return Pair.with(cleaned, nextToCheckAux);
+  }
+  
+  private Project getProject(String indexName) throws ProvenanceException {
+    int endIndex = indexName.indexOf(Settings.PROV_FILE_INDEX_SUFFIX);
+    String sInodeId = indexName.substring(0, endIndex);
+    Long inodeId;
+    try {
+      inodeId = Long.parseLong(sInodeId);
+    }catch(NumberFormatException e) {
+      throw new ProvenanceException(RESTCodes.ProvenanceErrorCode.INTERNAL_ERROR, Level.WARNING,
+        "error extracting project from prov index name - format error", e.getMessage(), e);
+    }
+    Inode inode = inodeFacade.findById(inodeId);
+    if(inode == null) {
+      return null;
+    }
+    Project project = projectFacade.findByInodeId(inode.getInodePK().getParentId(), inode.getInodePK().getName());
+    return project;
+  }
+  
+  private String[] getAllIndices() throws ProvenanceException {
+    try {
+      String indexRegex = "*" + Settings.PROV_FILE_INDEX_SUFFIX;
+      GetIndexRequest request = new GetIndexRequest(indexRegex);
+      GetIndexResponse response = client.mngIndexGet(request);
+      return response.getIndices();
+    } catch(ElasticException e) {
+      throw new ProvenanceException(RESTCodes.ProvenanceErrorCode.INTERNAL_ERROR, Level.WARNING,
+        "error querying elastic", e.getMessage(), e);
+    }
+  }
+  
+  private void deleteProvIndex(String indexName) {
+    DeleteIndexRequest request = new DeleteIndexRequest(indexName);
+    try {
+      AcknowledgedResponse response = client.mngIndexDelete(request);
+    } catch (ElasticException e) {
+      if(e.getCause() instanceof ElasticsearchException) {
+        ElasticsearchException ex = (ElasticsearchException)e.getCause();
+        if(ex.status() == RestStatus.NOT_FOUND) {
+          LOGGER.log(Level.INFO, "trying to delete index:{0} - does not exist", indexName);
+          return;
+        }
+      }
+      LOGGER.log(Level.WARNING, "trying to delete index:{0}", e.getStackTrace());
+    }
+  }
+}
